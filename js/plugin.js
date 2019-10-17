3a77a1dee4fa1141c6ffefd41db25f3c1c7dc109
mohamednajiub <m.emad550@gmail.com>
2019-10-17 12:43:31 +0200

    Refactor: start rebuild plugin.js file with pure js

--- js/plugin.js
+++ js/plugin.js
@@ -1,53 +1,71 @@
-$(document).ready(function() {
-  const canvas = $("#pixel_canvas");
-  makeGride();
+const canvas = document.getElementById('pixel_canvas');
+let gridWidth = document.getElementById('gridWidth').value,
+  gridHeight = document.getElementById('gridHeight').value,
+  color = document.getElementById('color').value;
 
-  // Submit New grid
-  $("button").click(function(e) {
-    e.preventDefault();
-    reset();
-    makeGride();
-  });
 
-  // Make grid
-  function makeGride() {
-    // Assign width and height
-    let width = $("#gridWidth").val(),
-      height = $("#gridHeight").val();
-
-    // Drawing the Grid
-    for (let y = 0; y < height; y++) {
-      const line = $("<tr></tr>");
-      $(canvas).append(line);
-      for (let x = 0; x < width; x++) {
-        line.append("<td></td>");
-      }
+makeGrid = () =>{
+  for (let row = 0; row < gridHeight; row++) {
+    const rows = document.createElement('tr');
+    canvas.appendChild(rows)
+    for (let col = 0; col < gridWidth; col++) {
+      const cell = document.createElement('td')
+      rows.appendChild(cell);
     }
-    // coloring the grid.
-    $("td").click(function() {
-      let color = $("#color").val();
-      $(this).css("background-color", color);
-      // Removung border from nested cells
-      $(this).css("border", "none");
-      $("tr").css("border", "none");
-    });
   }
+}
 
-  // Clearing old grid
-  $("input[type = 'button']").click(function(e) {
-    e.preventDefault();
-    clear();
-  });
+makeGrid();
+// $(document).ready(function() {
+//   const canvas = $("#pixel_canvas");
+//   makeGride();
 
-  // empty old grid
-  function reset() {
-    $(canvas).empty();
-  }
+//   // Submit New grid
+//   $("button").click(function(e) {
+//     e.preventDefault();
+//     reset();
+//     makeGride();
+//   });
 
-  // Removing colors
-  function clear() {
-    $("td").css("background-color", "transparent");
-    $("td").css("border", "1px solid #000");
-    $("tr").css("border", "1px solid #000");
-  }
-});
+//   // Make grid
+//   function makeGride() {
+//     // Assign width and height
+//     let width = $("#gridWidth").val(),
+//       height = $("#gridHeight").val();
+
+//     // Drawing the Grid
+//     for (let y = 0; y < height; y++) {
+//       const line = $("<tr></tr>");
+//       $(canvas).append(line);
+//       for (let x = 0; x < width; x++) {
+//         line.append("<td></td>");
+//       }
+//     }
+//     // coloring the grid.
+//     $("td").click(function() {
+//       let color = $("#color").val();
+//       $(this).css("background-color", color);
+//       // Removung border from nested cells
+//       $(this).css("border", "none");
+//       $("tr").css("border", "none");
+//     });
+//   }
+
+//   // Clearing old grid
+//   $("input[type = 'button']").click(function(e) {
+//     e.preventDefault();
+//     clear();
+//   });
+
+//   // empty old grid
+//   function reset() {
+//     $(canvas).empty();
+//   }
+
+//   // Removing colors
+//   function clear() {
+//     $("td").css("background-color", "transparent");
+//     $("td").css("border", "1px solid #000");
+//     $("tr").css("border", "1px solid #000");
+//   }
+// });
